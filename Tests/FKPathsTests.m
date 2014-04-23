28d9ebf54ccb65992a768209f6a55f84f481c9de
Philip Messlehner <mess.philip@gmail.com>
2014-04-23 12:59:39 +0200

    now using new iOSKit Methods

--- Tests/FKPathsTests.m
+++ Tests/FKPathsTests.m
@@ -3,19 +3,19 @@
 @implementation FKPathsTests
 
 - (void)testDocumentsPath {
-  STAssertTrue([FKDocumentsDirectory() containsString:@"Documents"], @"Documents directory is wrong");
+  STAssertTrue([FKDocumentsDirectory() fkit_containsString:@"Documents"], @"Documents directory is wrong");
 }
 
 - (void)testApplicationSupportPath {
-  STAssertTrue([FKApplicationSupportDirectory() containsString:@"Application Support"], @"Application Support directory is wrong");
+  STAssertTrue([FKApplicationSupportDirectory() fkit_containsString:@"Application Support"], @"Application Support directory is wrong");
 }
 
 - (void)testLibraryPath {
-  STAssertTrue([FKLibraryDirectory() containsString:@"Library"], @"Library directory is wrong");
+  STAssertTrue([FKLibraryDirectory() fkit_containsString:@"Library"], @"Library directory is wrong");
 }
 
 - (void)testCachePath {
-  STAssertTrue([FKCacheDirectory() containsString:@"Cache"], @"Cache directory is wrong");
+  STAssertTrue([FKCacheDirectory() fkit_containsString:@"Cache"], @"Cache directory is wrong");
 }
 
 @end
